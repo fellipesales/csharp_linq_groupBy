518191e3030ee8aa83708814981bf266ab1b4323
Fellipe Sales <fsales@rcasistemas.com.br>
2019-10-16 14:11:00 -0300

    inicial

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace TesteMarcorattiArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrayNumeros = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2, 8, 1, 3, 9, 2, 7, 6, 3, 1, 5, 7, 8, 9 };
+
+            //ImprmirArrayUmaLinha(arrayNumeros);
+
+            ImprmirArray(arrayNumeros);
+
+            Console.ReadKey();
+        }
+
+        private static void ImprmirArrayUmaLinha(int[] arrayNumeros)
+        {
+            arrayNumeros.GroupBy(x => x).Select(a => new { Texto = a.First().ToString() + " aparece " + a.Count().ToString() + (a.Count().Equals(1) ? " vez " : " vezes ") }).ToList().ForEach(n => Console.WriteLine(n.Texto));
+        }
+
+        private static void ImprmirArray(int[] arrayNumeros)
+        {
+            var count = arrayNumeros.Count();
+
+            var arrayGroup = arrayNumeros.GroupBy(x => x).Select(a => new
+            {
+                Texto = a.First().ToString() + " aparece " + a.Count().ToString() + (a.Count().Equals(1) ? " vez " : " vezes ")
+            }).ToArray();
+
+            foreach (var item in arrayGroup)
+            {
+                Console.WriteLine(item.Texto);
+            }
+        }
+    }
+}
